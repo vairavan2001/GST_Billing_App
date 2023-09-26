a1709abfac6f8ecf1af34f6e2d2d1fd6fb568362
Vairavan K <136603137+vairavan2001@users.noreply.github.com>
2023-09-26 12:46:46 +0530

    Delete src directory

--- src/Home.js
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import Home_Header from './Home_Header'
-export default function Home() {
-  return (
-       <div className="home_page">
-        <Home_Header/>
-       </div>
-  )
-}
--- src/Home_Header.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-export default function Home_Header() {
-  return (
-    <div className='home_header'>
-
-    </div>
-  )
-}
