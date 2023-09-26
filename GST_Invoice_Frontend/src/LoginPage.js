8368fc9ccc0bd7478d00491996bed44a06b451c5
Vairavan K <136603137+vairavan2001@users.noreply.github.com>
2023-09-26 13:20:32 +0530

    Update LoginPage.js

--- GST_Invoice_Frontend/src/LoginPage.js
+++ GST_Invoice_Frontend/src/LoginPage.js
@@ -2,31 +2,39 @@ import { useState } from "react";
 
 const LoginPage=()=>{
     const [value,setvalue]=useState('Login');
-    const [logincolor,setlogincolor]=useState('red')
+    const [logincolor,setlogincolor]=useState('linear-gradient(to right,#7EB1EC,#346EB0)')
     const [signupcolor,setsignupcolor]=useState('white')
     const[loginfont,setloginfont]=useState('white');
     const [signupfont,setsignupfont]=useState('black');
+    const [loginborder,setloginborder]=useState('none');
+    const [signupborder,setsignupborder]=useState('2px solid grey');
+    const [credencials,setcredencials]=useState([{email:"",password:""}]);
+    const [signupdetails,setsignupdetails]=useState([{email:"",password:"",confirmpassword:""}]);
 return(
-    //<div style={{position:"absolute",top:"0",width:"100%",height:"100%",background:"skyblue"}}>
-    <div style={{marginTop:"10%",width:"40%",marginLeft:"30%",borderRadius:"25px",background:"skyblue",padding:"0"}}>
-        {value==="Login"?(<h1 style={{textAlign:"center"}}>Login Page</h1>):(<h1 style={{textAlign:"center"}}>SignUp Now</h1>)}
-        <div style={{width:"60%",height:"45px",marginLeft:"20%",marginTop:"45px"}}>
-            <button style={{width:"50%",height:"45px",borderRadius:"5px",backgroundColor:logincolor,border:"2px solid grey",textAlign:"center",fontSize:"20px",color:loginfont}} onClick={(e)=>{e.preventDefault();setvalue('Login');setlogincolor("red");setsignupcolor("white");setloginfont("white");setsignupfont("black")}}>Login</button>
-            <button style={{width:"50%",height:"45px",borderRadius:"5px",backgroundColor:signupcolor,border:"2px solid grey",textAlign:"center",fontSize:"20px",color:signupfont}} onClick={(e)=>{e.preventDefault();setvalue('SignUp');setsignupcolor("red");setlogincolor("white");setloginfont("black");setsignupfont("white")}}>SignUp</button>
+    <div style={{position:"absolute",top:"0",width:"100%",height:"100%"}} className="bg-color">
+    <div style={{marginTop:"10%",width:"35%",marginLeft:"32.5%",borderRadius:"25px",background:"white",padding:"0"}}>
+        {value==="Login"?(<h1 style={{textAlign:"center",paddingTop:"45px"}}>Login Form</h1>):(<h1 style={{textAlign:"center",paddingTop:"45px"}}>Signup Form</h1>)}
+            <div style={{width:"70%",height:"45px",marginLeft:"15%",marginTop:"45px"}}>
+            <button style={{width:"50%",height:"45px",borderRadius:"5px",background:logincolor,border:loginborder,textAlign:"center",fontSize:"20px",color:loginfont}} onClick={(e)=>{e.preventDefault();setvalue('Login');setlogincolor("linear-gradient(to right,#7EB1EC,#346EB0)");setsignupcolor("white");setloginfont("white");setsignupfont("black");setloginborder("none");setsignupborder("2px solid grey")}}>Login</button>
+            <button style={{width:"50%",height:"45px",borderRadius:"5px",background:signupcolor,border:signupborder,textAlign:"center",fontSize:"20px",color:signupfont}} onClick={(e)=>{e.preventDefault();setvalue('SignUp');setsignupcolor("linear-gradient(to right,#7EB1EC,#346EB0)");setlogincolor("white");setloginfont("black");setsignupfont("white");setsignupborder("none");setloginborder("2px solid grey")}}>Signup</button>
+            </div>
             {
                 value==='Login'?(
-                    <form>
-                        <div style={{display:"block",width:"100%"}}>
-                        <input type="email" placeholder="Email Address" style={{width:"98%",marginBottom:"20px",height:"45px",marginTop:"20px",fontSize:"20px"}}/>
-                        <input type="password" placeholder="Password" style={{width:"98%",marginBottom:"20px",height:"45px",fontSize:"20px"}}/>
+                    <form method="post" onSubmit={null}>
+                        <div style={{display:"block",width:"70%",marginLeft:"15%",textAlign:"center"}}>
+                        <input type="email" placeholder="Email Address" style={{width:"98%",marginBottom:"20px",height:"45px",marginTop:"20px",fontSize:"20px"}} name="email" value={credencials[0]?.email} onChange={(e)=>setcredencials(arr=>arr.map((list,index)=>(index===0?{...list,[e.target.name]:e.target.value}:{...list})))}/>
+                        <input type="password" placeholder="Password" style={{width:"98%",marginBottom:"5px",height:"45px",fontSize:"20px"}} name="password" value={credencials[0]?.password} onChange={(e)=>setcredencials(arr=>arr.map((list,index)=>(index===0?{...list,[e.target.name]:e.target.value}:{...list})))}/>
+                        <div style={{textAlign:"left",marginBottom:"15px",}}><a style={{color:"#346EB0"}} href="">Forgot password?</a></div>
+                        <button type="submit" style={{width:"100%",height:"45px",background:"linear-gradient(to right,#7EB1EC,#346EB0)",borderRadius:"5px",marginBottom:"45px",fontSize:"20px",color:"white",border:"none"}}>Login</button>
                         </div>
                     </form>
                 ):(
                     <form>
-                         <div style={{display:"block",width:"100%"}}>
-                        <input type="email" placeholder="Email Address" style={{width:"98%",marginBottom:"20px",height:"45px",marginTop:"20px",fontSize:"20px"}}/>
-                        <input type="password" placeholder="Password" style={{width:"98%",marginBottom:"20px",height:"45px",fontSize:"20px"}}/>
-                        <input type="password" placeholder="Confirm Password" style={{width:"98%",marginBottom:"20px",height:"45px",fontSize:"20px"}}/>
+                        <div style={{display:"block",width:"70%",marginLeft:"15%",textAlign:"center"}}>
+                        <input type="email" placeholder="Email Address" style={{width:"98%",marginBottom:"20px",height:"45px",marginTop:"20px",fontSize:"20px"}} name="email" value={signupdetails[0].email} onChange={(e)=>setsignupdetails(arr=>arr.map((list,index)=>(index===0?{...list,[e.target.name]:e.target.value}:{...list})))}/>
+                        <input type="password" placeholder="Password" style={{width:"98%",marginBottom:"20px",height:"45px",fontSize:"20px"}} name="password" value={signupdetails[0].password} onChange={(e)=>setsignupdetails(arr=>arr.map((list,index)=>(index===0?{...list,[e.target.name]:e.target.value}:{...list})))}/>
+                        <input type="password" placeholder="Confirm Password" style={{width:"98%",marginBottom:"20px",height:"45px",fontSize:"20px"}} name="confirmpassword" value={signupdetails[0].confirmpassword} onChange={(e)=>setsignupdetails(arr=>arr.map((list,index)=>(index===0?{...list,[e.target.name]:e.target.value}:{...list})))}/>
+                        <button type="submit" style={{width:"100%",height:"45px",background:"linear-gradient(to right,#7EB1EC,#346EB0)",borderRadius:"5px",marginBottom:"45px",fontSize:"20px",color:"white",border:"none"}}>Signup</button>
                         </div>
                     </form>
                 )
@@ -34,7 +42,6 @@ return(
             }
         </div>
     </div>
-   // </div>
 );
 }
 export default LoginPage;
