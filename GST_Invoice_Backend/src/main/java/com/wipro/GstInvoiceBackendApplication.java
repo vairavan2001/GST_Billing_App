140ca09aabe8a02e10db5bc545f2541a7516ab2b
Vairavan <vairavan0719@gmail.com>
2023-09-26 11:27:38 +0530

    Initial setup

---
+++ GST_Invoice_Backend/src/main/java/com/wipro/GstInvoiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstInvoiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstInvoiceBackendApplication.class, args);
+	}
+
+}
